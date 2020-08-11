024cd63b3f4c927263300a1aa8e0c860fae9d79c
BeWorld <36823759+BeWorld2018@users.noreply.github.com>
2020-08-11 07:51:10 +0200

    Fix warning

--- MorphOS/devenv/sdl-startup.c
+++ MorphOS/devenv/sdl-startup.c
@@ -19,7 +19,7 @@ extern void __SDL2_OpenLibError(ULONG version, const char *name);
 #if defined(__NO_SDL_CONSTRUCTORS)
 extern struct Library *SDL2MixerBase;
 #else
-void _INIT_4_SDL2MixerBase(void) __attribute__((alias("__CSTP_init_SDL2MixerBase")));
+int _INIT_4_SDL2MixerBase(void) __attribute__((alias("__CSTP_init_SDL2MixerBase")));
 void _EXIT_4_SDL2MixerBase(void) __attribute__((alias("__DSTP_cleanup_SDL2MixerBase")));
 
 struct Library *SDL2MixerBase;
